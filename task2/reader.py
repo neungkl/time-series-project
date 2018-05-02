f93ed98d2b6c654f544692c68a753fc0cbf22715
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2018-05-02 16:32:37 +0700

    Init shape avg

---
+++ task2/reader.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def get_class_x(train_file_name):
+    train_data = open('./data/' + train_file_name, 'r')
+    train_x = list()
+    train_label = list()
+    
+    # Read data
+    for i, line in enumerate(train_data): 
+        array = line.split()
+        train_label.append(int(float(array[0])))
+        train_x.append(np.array(array[1:]).astype(float))
+
+    train_label = np.array(train_label).reshape((len(train_label)))
+    train_x = np.array(train_x).reshape((len(train_x), train_x[0].shape[0]))
+
+    # Collect data in class
+    classes = np.unique(train_label)
+    class_x = [list() for i in classes]
+    for i in range(train_label.shape[0]):
+        class_x[train_label[i]-1].append(np.array(train_x[i]))
+    class_x = np.array(class_x)
+    return class_x
